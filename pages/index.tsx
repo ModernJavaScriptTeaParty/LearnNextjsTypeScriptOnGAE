78602e9e80d1c5276c82fd94fe153ad8031406e2
Nobufumi Yoshida <nobufumi.yoshida@gmail.com>
2022-08-07 22:54:54 +0900

    202208082254 text p39 commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,20 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const subtitle = "<アプトプットしていくサイト>"
   return (
-    <h1>CUBE</h1>
+    <>
+    <header>HEADER</header>
+
+    <main>
+      <div className="hero">
+        <h1 style={{ color: 'red',fontSize: '80px' }}>CUBE</h1>
+        <p>{subtitle}</p>
+      </div>
+    </main>
+   
+    <footer>FOOTER</footer>
+    </>
  )
 }
 
