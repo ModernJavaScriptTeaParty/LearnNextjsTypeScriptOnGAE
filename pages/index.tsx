86bde6da90022545c34ef50c386013b136413b96
Nobufumi Yoshida <nobufumi.yoshida@gmail.com>
2022-08-08 22:45:08 +0900

    styles set cheage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from 'styles/Home.module.css'
 import Header from 'components/header'
 import Hero from 'components/hero'
 import Footer from 'components/footer'
