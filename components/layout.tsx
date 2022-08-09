c0b64be2fb541d68c58fa87d632850f56a5d7d22
Nobufumi Yoshida <nobufumi.yoshida@gmail.com>
2022-08-09 20:30:31 +0900

    layont set

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,11 @@
+import { ReactNode } from 'react'
 import Header from 'components/header'
 import Footer from 'components/footer'
+type Props = {
+  children: ReactNode;
+}
 
-export default function Layout( {children} ) {
+export default function Layout( {children}: Props ) {
     return(
         <>
             <Header />
