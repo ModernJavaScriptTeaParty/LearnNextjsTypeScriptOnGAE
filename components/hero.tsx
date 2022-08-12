466a67a8be8f5cf2755a3f49809d8967a00db920
Nobufumi Yoshida <nobufumi.yoshida@gmail.com>
2022-08-12 16:38:26 +0900

    global stayle set 202208121638

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,3 @@
-import styles from 'styles/hero.module.css'
-
 type Props = {
     title: string;
     subtitle: string;
@@ -8,9 +6,9 @@ type Props = {
 
 export default function Hero( { title, subtitle, imageOn = false }: Props) {
     return (
-        <div className={styles.text}>
-            <h1 className={styles.title}>{title}</h1>
-            <p className={styles.subtitle}>{subtitle}</p>
+        <div>
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
             {imageOn && <figure>[画像]</figure>} 
         </div>
     )
