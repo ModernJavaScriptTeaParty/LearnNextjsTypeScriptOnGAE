8f2a20568dd782224c27ab0f61fcaa360d24f41b
Nobufumi Yoshida <nobufumi.yoshida@gmail.com>
2022-08-27 12:40:43 +0900

    P117 conteiner add

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,15 +3,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from 'styles/Home.module.css'
 
+import Container from 'components/container'
 import Hero from   'components/hero'
 
 const Home: NextPage = () => {
   return (
-    <Hero 
-      title = "About"
-      subtitle ="About development activities"
-      imageOn = {false}
-    />
+    <Container>
+      <Hero 
+        title = "About"
+        subtitle ="About development activities"
+        imageOn = {false}
+      />
+    </Container>
  )
 }
 
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,15 +3,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from 'styles/Home.module.css'
 
+import Container from 'components/container'
 import Hero from   'components/hero'
 
 const Home: NextPage = () => {
   return (
-    <Hero 
-      title = "Blog" 
-      subtitle = "React Posts"
-      imageOn = {false}
-    />
+    <Container>
+      <Hero 
+        title = "Blog" 
+        subtitle = "React Posts"
+        imageOn = {false}
+      />
+    </Container>
  )
 }
 
