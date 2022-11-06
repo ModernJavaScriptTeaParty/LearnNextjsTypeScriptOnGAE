b95ea17e83aa5ecd8288dd9cf630566347092fd6
Nobufumi Yoshida <nobufumi.yoshida@gmail.com>
2022-11-06 17:21:19 +0900

    P191 site const set

---
+++ lib/constants.tsx
@@ -0,0 +1,9 @@
+export const siteMeta = {
+    siteTitle: 'CUBE',
+    siteDesc: 'アプトプットしていくサイト',
+    siteUrl:'https://icutok.jp',
+    siteLang:'ja',
+    siteLocation:'ja_JP',
+    siteType:'wetsite',
+    siteIcon:'/favicon.png',
+}
