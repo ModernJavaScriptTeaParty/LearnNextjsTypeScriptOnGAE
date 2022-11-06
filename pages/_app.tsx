95d079c914067c5cb5b5f5aa0b6f8b23f9cc3fa9
Nobufumi Yoshida <nobufumi.yoshida@gmail.com>
2022-11-06 15:23:50 +0900

    P181 Social icon add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import 'styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from 'components/layout'
 
+/* font awesome の設定 */
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
