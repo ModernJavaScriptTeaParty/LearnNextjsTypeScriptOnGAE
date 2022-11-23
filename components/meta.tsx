c37958156e65837ed142c37c5c45fb1fe57601c0
Nobufumi Yoshida <nobufumi.yoshida@gmail.com>
2022-11-23 16:37:29 +0900

    P197 add meta icon etc

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 // サイトに関する情報
 import {siteMeta } from 'lib/constants'
+import { useRouter } from 'next/router'
 const { siteTitle, siteDesc, siteUrl, siteLocation, siteType, siteIcon } = siteMeta
 let desc : string
 
@@ -17,6 +18,10 @@ export default function Meta( { pageTitle , pageDesc }: Props ) {
     } else {
         desc = pageDesc
     }
+    // ページのURL
+    const router = useRouter()
+    const url = `${siteUrl}${router.asPath}`
+
     return (
         <Head>
             <title>{title}</title>
@@ -24,6 +29,16 @@ export default function Meta( { pageTitle , pageDesc }: Props ) {
 
             <meta name="description" content={desc} />
             <meta property='og:description' content={desc} />
+
+            <link rel="canonical" href={url} />
+            <meta property="og:url" content={url} />
+            
+            <meta property="og:site_name" content={siteTitle} />
+            <meta property="og:type" content={siteType} />
+            <meta property="og:location" content={siteLocation} />
+
+            <link rel="icon" href={siteIcon} />
+            <link rel="apple-touch-icon" href={siteIcon} />
         </Head>
     )
 }
